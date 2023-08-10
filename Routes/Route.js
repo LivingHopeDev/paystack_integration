976c920a74e6f93b6af31f07ce8144243c7bb876
adewobiadetayo1@gmail.com <adewobiadetayo1@gmail.com>
2023-08-10 04:28:08 +0100

    refined

--- Routes/Route.js
+++ Routes/Route.js
@@ -17,10 +17,6 @@ router.route("/user/register").post(register);
 router.route("/user/login").post(login);
 router.route("/user/logout").get(verifyToken, logout);
 
-router.route("/verify-email").post(emailVerification);
-router.route("/resendOTP").post(resendVerificationEmail);
-router.route("/passwordResetEmail").post(passwordResetEmail);
-router.route("/resetPassword").post(resetPassword);
 
 router.route("/payment").post(payment);
 router.route("/paymentVerification").post(verifyPayment);
